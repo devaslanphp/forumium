da38dba5221995e23b661a81c33b16765f47bcfc
EL OUFIR Hatim <eloufirhatim@gmail.com>
2022-12-22 10:10:21 +0100

    Design enhancement

--- resources/views/livewire/just-registered.blade.php
+++ resources/views/livewire/just-registered.blade.php
@@ -6,7 +6,7 @@
     <span class="text-slate-600 max-w-[80%]">
         We're happy you're here. Check your inbox to verify your email address before you can use your account.
     </span>
-    <button type="button" wire:click="resend()" class="w-fit text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+    <button type="button" wire:click="resend()" wire:loading.attr="disabled" class="w-fit mt-6 text-white bg-blue-700 disabled:bg-slate-300 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
         Resent verification email
     </button>
     <span class="text-slate-600 text-sm max-w-[80%]">
--- resources/views/livewire/layout/dialogs/register.blade.php
+++ resources/views/livewire/layout/dialogs/register.blade.php
@@ -1,6 +1,6 @@
 <form wire:submit.prevent="register">
     {{ $this->form }}
-    <button type="submit" class="mt-6 w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+    <button type="submit" wire:loading.attr="disabled" class="mt-6 w-full text-white bg-blue-700 disabled:bg-slate-300 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
         Create your account
     </button>
 </form>
