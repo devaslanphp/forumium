22728116174b005d74fb8b4cd9553f7571413635
EL OUFIR Hatim <eloufirhatim@gmail.com>
2022-12-24 15:38:20 +0100

    App enhancement

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -18,9 +18,14 @@
                 <nav class="mt-5 overflow-auto">
                     <div class="flex-grow md:block md:overflow-y-auto overflow-x-hidden" :class="{'opacity-1': sidebarOpen, 'opacity-0': !sidebarOpen}">
                         <div class="flex-col w-full px-5 mt-5">
-                            <button type="button" data-modal-toggle="add-discussion-modal" class="bg-blue-500 hover:bg-blue-600 hover:cursor-pointer px-3 py-2 rounded shadow hover:shadow-lg text-white font-medium text-center">
-                                Start a Discussion
-                            </button>
+                            @if(
+                                (auth()->user() && auth()->user()->hasVerifiedEmail()) &&
+                                auth()->user()->can(Permissions::START_DISCUSSIONS->value)
+                            )
+                                <button type="button" data-modal-toggle="add-discussion-modal" class="bg-blue-500 hover:bg-blue-600 hover:cursor-pointer px-3 py-2 rounded shadow hover:shadow-lg text-white font-medium text-center">
+                                    Start a Discussion
+                                </button>
+                            @endif
                             <div class="w-full flex flex-col gap-5 my-8">
                                 <a href="/discussions.html" class="w-full flex items-center text-blue-500 font-medium">
                                     <span class="w-[30px]"><i class="fa-regular fa-comments"></i></span>
