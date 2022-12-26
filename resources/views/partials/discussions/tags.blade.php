aea5623846d57c6bac199d058e7477687f78d29f
EL OUFIR Hatim <eloufirhatim@gmail.com>
2022-12-26 18:10:18 +0100

    Design enhancement

--- resources/views/partials/discussions/tags.blade.php
+++ resources/views/partials/discussions/tags.blade.php
@@ -1,6 +1,6 @@
 <div class="flex items-center gap-1">
     @foreach($tags as $tag)
-        <span class="text-sm font-medium px-2.5 py-0.5 rounded"
+        <span class="min-w-[70px] w-fit text-xs font-medium px-2.5 py-0.5 rounded flex items-center justify-center text-center"
               style="background-color: {{ ($loop->index == 0 && !isset($ignore_first)) ? '#ffffff' : $tag->color }}; color: {{ ($loop->index == 0 && !isset($ignore_first)) ? $tag->color : '#ffffff' }}">
             {{ $tag->name }}
         </span>
