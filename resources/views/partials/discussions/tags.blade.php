8f14fcbaceca19b5723b5356485ed754705d636b
EL OUFIR Hatim <eloufirhatim@gmail.com>
2022-12-26 18:23:13 +0100

    Design / App enhancement

--- resources/views/partials/discussions/tags.blade.php
+++ resources/views/partials/discussions/tags.blade.php
@@ -1,6 +1,6 @@
 <div class="flex items-center gap-1">
     @foreach($tags as $tag)
-        <span class="min-w-[70px] w-fit text-xs font-medium px-2.5 py-0.5 rounded flex items-center justify-center text-center"
+        <span onclick="window.location.href ='{{ route('tag', ['tag' => $tag, 'slug' => Str::slug($tag->name)]) }}'" class="min-w-[70px] w-fit text-xs font-medium px-2.5 py-0.5 rounded flex items-center justify-center text-center hover:cursor-pointer"
               style="background-color: {{ ($loop->index == 0 && !isset($ignore_first)) ? '#ffffff' : $tag->color }}; color: {{ ($loop->index == 0 && !isset($ignore_first)) ? $tag->color : '#ffffff' }}">
             {{ $tag->name }}
         </span>
