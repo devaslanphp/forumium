860addc54041e8114f6d309f272dfa9121e88ab8
EL OUFIR Hatim <eloufirhatim@gmail.com>
2023-01-02 13:55:41 +0100

    Add email notifications

--- app/Jobs/DispatchNotificationsJob.php
+++ app/Jobs/DispatchNotificationsJob.php
@@ -7,6 +7,7 @@ use App\Core\NotificationConstants;
 use App\Models\Comment;
 use App\Models\Discussion;
 use App\Models\Reply;
+use App\Notifications\EmailNotification;
 use Filament\Notifications\Actions\Action;
 use Filament\Notifications\Notification;
 use Illuminate\Bus\Queueable;
@@ -48,102 +49,115 @@ class DispatchNotificationsJob implements ShouldQueue
         $url = null;
         $recipient = null;
 
-        if ($this->user->hasNotification($this->notification, true, false)) {
-            switch ($this->notification) {
-                case NotificationConstants::MY_DISCUSSION_EDITED->value:
-                    // $object = Discussion
-                    if ($this->object->user->id != $this->user->id) {
-                        $title = 'Discussion edited';
-                        $body = $this->user->name . ' edited your discussion ' . $this->object->name;
-                        $url = route('discussion', ['discussion' => $this->object, 'slug' => Str::slug($this->object->name)]);
-                        $recipient = $this->object->user;
-                    }
-                    break;
-                case NotificationConstants::POST_IN_DISCUSSION->value:
-                    // $this->object = Discussion
-                    if ($this->object) {
-                        $users = $this->object->followers()->where('type', FollowerConstants::FOLLOWING->value)->get();
-                        foreach ($users as $u) {
-                            if ($this->user->id != $u->id) {
-                                $title = 'Discussion updated';
-                                $body = 'A new activity has been done by ' . $this->user->name . ' into the discussion ' . $this->object->name;
-                                $url = route('discussion', ['discussion' => $this->object, 'slug' => Str::slug($this->object->name)]);
-                                $recipient = $u;
-                            }
+        switch ($this->notification) {
+            case NotificationConstants::MY_DISCUSSION_EDITED->value:
+                // $object = Discussion
+                if ($this->object->user->id != $this->user->id) {
+                    $title = 'Discussion edited';
+                    $body = $this->user->name . ' edited your discussion ' . $this->object->name;
+                    $url = route('discussion', ['discussion' => $this->object, 'slug' => Str::slug($this->object->name)]);
+                    $recipient = $this->object->user;
+                    $this->sendNotification($title, $body, $recipient, $url);
+                }
+                break;
+            case NotificationConstants::POST_IN_DISCUSSION->value:
+                // $this->object = Discussion
+                if ($this->object) {
+                    $users = $this->object->followers()->where('type', FollowerConstants::FOLLOWING->value)->get();
+                    foreach ($users as $u) {
+                        if ($this->user->id != $u->id) {
+                            $title = 'Discussion updated';
+                            $body = 'A new activity has been done by ' . $this->user->name . ' into the discussion ' . $this->object->name;
+                            $url = route('discussion', ['discussion' => $this->object, 'slug' => Str::slug($this->object->name)]);
+                            $recipient = $u;
+                            $this->sendNotification($title, $body, $recipient, $url);
                         }
                     }
-                    break;
-                case NotificationConstants::MY_REPLY_BEST_ANSWER->value:
-                    // $this->object = Reply
-                    if ($this->object->user->id != $this->user->id) {
-                        $title = 'Best answer!';
-                        $body = $this->user->name . ' marked your reply as best answer';
-                        $url = route('discussion', ['discussion' => $this->object->discussion, 'slug' => Str::slug($this->object->name), 'r' => $this->object->id]);
-                        $recipient = $this->object->user;
-                    }
-                    break;
-                case NotificationConstants::MY_POSTS_COMMENTED->value:
-                    // $this->object = Comment
-                    if ($this->object->user->id != $this->user->id) {
-                        if ($this->object->source instanceof Discussion) {
-                            $title = 'Discussion commented';
-                            $body = $this->user->name . ' added a comment to your discussion';
-                            $url = route('discussion', ['discussion' => $this->object->discussion, 'slug' => Str::slug($this->object->name), 'd' => $this->object->source->id, 'c' => $this->object->id]);
-                        } elseif ($this->object->source instanceof Reply) {
-                            $title = 'Reply commented';
-                            $body = $this->user->name . ' added a comment to your reply';
-                            $url = route('discussion', ['discussion' => $this->object->discussion, 'slug' => Str::slug($this->object->name), 'd' => $this->object->source->source->id, 'r' => $this->object->source->id, 'c' => $this->object->id]);
-                        }
-                        $recipient = $this->object->user;
-                    }
-                    break;
-                case NotificationConstants::MY_POSTS_LIKED->value:
-                    // $this->object = Like
+                }
+                break;
+            case NotificationConstants::MY_REPLY_BEST_ANSWER->value:
+                // $this->object = Reply
+                if ($this->object->user->id != $this->user->id) {
+                    $title = 'Best answer!';
+                    $body = $this->user->name . ' marked your reply as best answer';
+                    $url = route('discussion', ['discussion' => $this->object->discussion, 'slug' => Str::slug($this->object->name), 'r' => $this->object->id]);
+                    $recipient = $this->object->user;
+                    $this->sendNotification($title, $body, $recipient, $url);
+                }
+                break;
+            case NotificationConstants::MY_POSTS_COMMENTED->value:
+                // $this->object = Comment
+                if ($this->object->user->id != $this->user->id) {
                     if ($this->object->source instanceof Discussion) {
-                        $title = 'Discussion liked';
-                        $body = $this->user->name . ' liked your discussion';
-                        $url = route('discussion', ['discussion' => $this->object->source, 'slug' => Str::slug($this->object->source->name), 'd' => $this->object->source->id, 'l' => $this->object->id]);
-                        $recipient = $this->object->source->user;
+                        $title = 'Discussion commented';
+                        $body = $this->user->name . ' added a comment to your discussion';
+                        $url = route('discussion', ['discussion' => $this->object->discussion, 'slug' => Str::slug($this->object->name), 'd' => $this->object->source->id, 'c' => $this->object->id]);
                     } elseif ($this->object->source instanceof Reply) {
-                        $title = 'Reply liked';
-                        $body = $this->user->name . ' liked your reply';
-                        $url = route('discussion', ['discussion' => $this->object->source->discussion, 'slug' => Str::slug($this->object->source->discussion->name), 'd' => $this->object->source->discussion->id, 'r' => $this->object->source->id, 'l' => $this->object->id]);
-                        $recipient = $this->object->source->user;
-                    } elseif ($this->object->source instanceof Comment) {
-                        $title = 'Comment liked';
-                        $body = $this->user->name . ' liked your comment';
-                        if ($this->object->source->source instanceof Discussion) {
-                            $url = route('discussion', ['discussion' => $this->object->source->source, 'slug' => Str::slug($this->object->source->source->name), 'd' => $this->object->source->source->id, 'c' => $this->object->source->id, 'l' => $this->object->id]);
-                            $recipient = $this->object->source->source->user;
-                        } elseif ($this->object->source->source instanceof Reply) {
-                            $url = route('discussion', ['discussion' => $this->object->source->source->discussion, 'slug' => Str::slug($this->object->source->source->discussion->name), 'd' => $this->object->source->source->discussion->id, 'r' => $this->object->source->source->id, 'c' => $this->object->source->id, 'l' => $this->object->id]);
-                            $recipient = $this->object->source->source->user;
-                        }
+                        $title = 'Reply commented';
+                        $body = $this->user->name . ' added a comment to your reply';
+                        $url = route('discussion', ['discussion' => $this->object->discussion, 'slug' => Str::slug($this->object->name), 'd' => $this->object->source->source->id, 'r' => $this->object->source->id, 'c' => $this->object->id]);
                     }
-                    break;
-                case NotificationConstants::POINTS_UPDATED->value:
-                    // $this->object = array (containing 'added' and 'current')
-                    $title = 'Points updated';
-                    $body = 'Your points are updated (' . ($this->object['added'] > 0 ? '+' : '-') . $this->object['added'] . '), you have now ' . $this->object['current'];
-                    $url = route('profile.index');
-                    $recipient = $this->user;
-                    break;
-                case NotificationConstants::DISCUSSION_LOCKED->value:
-                    // $this->object = Discussion
-                    $title = 'Discussion locked';
-                    $body = 'Your discussion ' . $this->object->name . ' is now locked by ' . $this->user->name;
-                    $url = route('discussion', ['discussion' => $this->object, 'slug' => Str::slug($this->object->name)]);
                     $recipient = $this->object->user;
-                    break;
-                case NotificationConstants::DISCUSSION_UNLOCKED->value:
-                    // $this->object = Discussion
-                    $title = 'Discussion unlocked';
-                    $body = 'Your discussion ' . $this->object->name . ' is now unlocked by ' . $this->user->name;
-                    $url = route('discussion', ['discussion' => $this->object, 'slug' => Str::slug($this->object->name)]);
-                    $recipient = $this->object->user;
-                    break;
-            }
-            if ($title && $body && $recipient) {
+                    $this->sendNotification($title, $body, $recipient, $url);
+                }
+                break;
+            case NotificationConstants::MY_POSTS_LIKED->value:
+                // $this->object = Like
+                if ($this->object->source instanceof Discussion) {
+                    $title = 'Discussion liked';
+                    $body = $this->user->name . ' liked your discussion';
+                    $url = route('discussion', ['discussion' => $this->object->source, 'slug' => Str::slug($this->object->source->name), 'd' => $this->object->source->id, 'l' => $this->object->id]);
+                    $recipient = $this->object->source->user;
+                } elseif ($this->object->source instanceof Reply) {
+                    $title = 'Reply liked';
+                    $body = $this->user->name . ' liked your reply';
+                    $url = route('discussion', ['discussion' => $this->object->source->discussion, 'slug' => Str::slug($this->object->source->discussion->name), 'd' => $this->object->source->discussion->id, 'r' => $this->object->source->id, 'l' => $this->object->id]);
+                    $recipient = $this->object->source->user;
+                } elseif ($this->object->source instanceof Comment) {
+                    $title = 'Comment liked';
+                    $body = $this->user->name . ' liked your comment';
+                    if ($this->object->source->source instanceof Discussion) {
+                        $url = route('discussion', ['discussion' => $this->object->source->source, 'slug' => Str::slug($this->object->source->source->name), 'd' => $this->object->source->source->id, 'c' => $this->object->source->id, 'l' => $this->object->id]);
+                        $recipient = $this->object->source->source->user;
+                    } elseif ($this->object->source->source instanceof Reply) {
+                        $url = route('discussion', ['discussion' => $this->object->source->source->discussion, 'slug' => Str::slug($this->object->source->source->discussion->name), 'd' => $this->object->source->source->discussion->id, 'r' => $this->object->source->source->id, 'c' => $this->object->source->id, 'l' => $this->object->id]);
+                        $recipient = $this->object->source->source->user;
+                    }
+                }
+                $this->sendNotification($title, $body, $recipient, $url);
+                break;
+            case NotificationConstants::POINTS_UPDATED->value:
+                // $this->object = array (containing 'added' and 'current')
+                $title = 'Points updated';
+                $body = 'Your points are updated (' . ($this->object['added'] > 0 ? '+' : '-') . $this->object['added'] . '), you have now ' . $this->object['current'];
+                $url = route('profile.index');
+                $recipient = $this->user;
+                $this->sendNotification($title, $body, $recipient, $url);
+                break;
+            case NotificationConstants::DISCUSSION_LOCKED->value:
+                // $this->object = Discussion
+                $title = 'Discussion locked';
+                $body = 'Your discussion ' . $this->object->name . ' is now locked by ' . $this->user->name;
+                $url = route('discussion', ['discussion' => $this->object, 'slug' => Str::slug($this->object->name)]);
+                $recipient = $this->object->user;
+                $this->sendNotification($title, $body, $recipient, $url);
+                break;
+            case NotificationConstants::DISCUSSION_UNLOCKED->value:
+                // $this->object = Discussion
+                $title = 'Discussion unlocked';
+                $body = 'Your discussion ' . $this->object->name . ' is now unlocked by ' . $this->user->name;
+                $url = route('discussion', ['discussion' => $this->object, 'slug' => Str::slug($this->object->name)]);
+                $recipient = $this->object->user;
+                $this->sendNotification($title, $body, $recipient, $url);
+                break;
+        }
+    }
+
+    private function sendNotification(string $title, string $body, $recipient, string|null $url)
+    {
+        if ($title && $body && $recipient) {
+            // Via web
+            if ($this->user->hasNotification($this->notification, true, false)) {
                 $actions = [];
                 if ($url) {
                     $actions = [
@@ -160,6 +174,10 @@ class DispatchNotificationsJob implements ShouldQueue
                     ->actions($actions ?? [])
                     ->sendToDatabase($recipient);
             }
+        }
+        // Via email
+        if ($this->user->hasNotification($this->notification, false, true)) {
+            $this->user->notify(new EmailNotification($title, $body, $recipient, $url));
         }
     }
 }
---
+++ app/Notifications/EmailNotification.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class EmailNotification extends Notification
+{
+    use Queueable;
+
+    private string $title;
+    private string $body;
+    private $recipient;
+    private string|null $url;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct(string $title, string $body, $recipient, string|null $url)
+    {
+        $this->title = $title;
+        $this->body = $body;
+        $this->recipient = $recipient;
+        $this->url = $url;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param mixed $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param mixed $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        $mail = (new MailMessage)
+            ->subject($this->title)
+            ->line($this->body);
+        if ($this->url) {
+            $mail->action('More details', $this->url);
+        }
+        $mail->line('Thank you for using ' . config('app.name') . '!');
+        return $mail;
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param mixed $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
